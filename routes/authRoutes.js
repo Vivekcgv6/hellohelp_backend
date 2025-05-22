aa2f275b5044bb3eef7b1f684e690b5036ef9ec6
Vivekcgv6 <vivekcgv6@gmail.com>
2025-05-22 15:58:25 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, getUserInfo } = require('../controllers/authController');
+const authenticateToken = require('../middleware/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/user-info', authenticateToken, getUserInfo);
 
 module.exports = router;
